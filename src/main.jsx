2bc89e4c3845e585ee1cb328ce83673265d11631
NLM cours <nlmcours@MacBook-Air-de-Nathan.local>
2023-12-08 02:07:31 +0100

    first version of the bike game

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './pages/App'
+import Velo from './pages/Velo'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Velo />
   </React.StrictMode>,
 )
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import StartButton from '../components/StartButton'
 
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <>
-      
+      <StartButton></StartButton>
       
     </>
   )
