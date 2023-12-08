18a31129da7aff5633c741852b6ee281039ebeac
mznluppio <119616374+mznluppio@users.noreply.github.com>
2023-12-08 14:27:35 +0100

    Update Tri.jsx

--- src/pages/Tri.jsx
+++ src/pages/Tri.jsx
@@ -1,6 +1,6 @@
 
 export default function Tri({ handleTriFinish }) {
     return (
-        <iframe src="https://ndl.alwaysdata.net/" title="Contenu HTML" width="100%" height="1080" />
+        <iframe src="https://ndl.alwaysdata.net/" title="Contenu HTML" width="80%" height="1000" />
     );
 }
