078843890ea4d9f9a7a69bc6c386166bd9a16f2a
Lilian-Monard <118742747+Lilian-Monard@users.noreply.github.com>
2023-12-08 14:12:58 +0100

    Update Tri.jsx

--- src/pages/Tri.jsx
+++ src/pages/Tri.jsx
@@ -1,6 +1,6 @@
 
 export default function Tri({ handleTriFinish }) {
     return (
-        <iframe src="src\pages\index.html" title="Contenu HTML" width="100%" height="1080" />
+        <iframe src="src/pages/index.html" title="Contenu HTML" width="100%" height="1080" />
     );
 }
